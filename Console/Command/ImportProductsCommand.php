b2bfa5ffbd55c1666e1ab8a4f93029a72957e7b9
Ciprian <ciprian.trusca@gmail.com>
2019-02-17 14:14:49 +0200

    first commit

---
+++ Console/Command/ImportProductsCommand.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Sagital\ProductImporter\Console\Command;
+
+use Magento\Framework\Filesystem\DirectoryList;
+use Symfony\Component\Console\Command\Command;
+use Magento\Framework\App\State;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\ImportExport\Model\Import\ErrorProcessing\ProcessingErrorAggregatorInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use \Magento\ImportExport\Model\Import;
+
+class ImportProductsCommand extends Command
+{
+
+    /**
+     * @var ObjectManagerInterface $objectManager
+     */
+    protected $objectManager;
+
+
+    /**
+     * @var LoggerInterface $logger ;
+     */
+    protected $logger;
+
+    /**
+     * @var State $state
+     */
+    protected $state;
+
+
+    const FILE_ARGUMENT = 'File';
+    const IMAGES_PATH_ARGUMENT = 'Images Path';
+
+    public function __construct(ObjectManagerInterface $objectManager, LoggerInterface $logger, State $state)
+    {
+
+        $this->logger = $logger;
+        $this->state = $state;
+        $this->objectManager = $objectManager;
+
+        parent::__construct();
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('sagital:import')
+            ->setDescription('Import products from a CSV file')
+            ->setDefinition([
+                new InputArgument(
+                    self::FILE_ARGUMENT,
+                    InputArgument::REQUIRED,
+                    'Products CSV file location'
+                ),
+                new InputArgument(
+                    self::IMAGES_PATH_ARGUMENT,
+                    InputArgument::OPTIONAL,
+                    'The location of the images folder',
+                    'pub/media/import/'
+                ),
+
+            ]);
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $this->state->setAreaCode('adminhtml');
+        $filesystem = $this->objectManager
+            ->create(\Magento\Framework\Filesystem::class);
+
+        $file = $input->getArgument(self::FILE_ARGUMENT);
+        $imagesPath = $input->getOption(self::IMAGES_PATH_ARGUMENT);
+
+        $directory = $filesystem->getDirectoryWrite(DirectoryList::ROOT);
+
+        $source = $this->objectManager->create(
+            \Magento\ImportExport\Model\Import\Source\Csv::class,
+            [
+                'file' => $file,
+                'directory' => $directory
+            ]
+        );
+
+        $import = $this->objectManager->create(
+            Import::class,
+            ['logger' => $this->logger]
+        );
+
+        $import->addData(
+            ['behavior' => Import::BEHAVIOR_APPEND,
+                Import::FIELD_NAME_IMG_FILE_DIR => $imagesPath,
+                Import::FIELD_NAME_VALIDATION_STRATEGY => ProcessingErrorAggregatorInterface::VALIDATION_STRATEGY_STOP_ON_ERROR,
+                'entity' => 'catalog_product'])
+            ->validateSource($source);
+
+        $import->importSource();
+
+        $logTrace = $import->getFormatedLogTrace();
+
+        $output->writeln($logTrace);
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+### About
+
+This is a CLI alternative to importing products using the Administrative HTTP interface.
+
+
+
+### Usage
+
+        magento sagital:import-products /path/to/products.csv [path/to/images] 
